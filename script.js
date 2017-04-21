5f528ec214baa2e245d5f3b2894b9eee6773e218
Diana Cholankeril <dianac@id.iit.edu>
2017-04-21 00:01:06 -0500

    i did it in a separate folder that didn’t push for whatever reason, so I’m doing it here again in one go. i can show you in class the other folder.

--- script.js
+++ script.js
@@ -1,2 +1,66 @@
 // Do your work in this file.
 
+// Do your work in this file.
+
+
+// var element = ["view", "grass", "sun", "mountain", "mountain-top"];
+
+
+var divView = document.createElement("div");
+divView.className = "view";
+document.body.appendChild(divView);
+
+var divGrass = document.createElement("div");
+divGrass.className = "grass";
+divView.appendChild(divGrass);
+
+var divSun = document.createElement("div");
+divSun.className = "sun";
+divView.appendChild(divSun);
+
+var divMountain = document.createElement("div");
+divMountain.className = "mountain";
+divView.appendChild(divMountain);
+
+var divMountaintop = document.createElement("div");
+divMountaintop.className = "mountain-top";
+divMountain.appendChild(divMountaintop);
+
+
+
+var t = 1;
+while (t < 6) {
+    var divRay = document.createElement("div");
+    divRay.className = "ray-" + t;
+    divSun.appendChild(divRay);
+    t++;
+}
+
+var m = 1;
+do {
+    var divMountaincap = document.createElement("div");
+    divMountaincap.className = "mountain-cap-" + m;
+    divMountain.appendChild(divMountaincap);
+    m++;
+} while (m < 4);
+
+// while (t < 4) {
+//     addASingleDivYo();
+//     t++;
+// }
+
+// var t = 1;
+// do {
+//    ray-1();
+//    t++;
+// } while (t < 6);
+
+
+
+// var text = "";
+// var i = 0;
+// do {
+//     text += "The number is " + i;
+//     i++;
+// }
+// while (i < 5);
